6b6610314efcc6b333f625dc4b3de75f42874e94
unknown <meire.isaac1@gmail.com>
2023-01-02 23:39:34 -0300

    falta exibir as mensagens

--- index.php
+++ index.php
@@ -109,6 +109,118 @@ include('conexao.php');
 				</script>		
 			</header>
 
+			<?php 
+
+					if(isset($_POST['add'])){ ?>
+						<section class='box-amigo-add'>
+							<form name='add' method='POST'>
+								<button name="fechar" class="btn-fechar">X</button>
+								<h2>Insira o apelido do seu amigo:</h2> <br><br>
+								<input type='text' name='apelido' class="
+								input-amigos-add"> <br><br>
+								<input type='submit' name='adicionar' value='Adicionar' class="btn-adicionar"> <br> <br>
+
+								<img style="width: 300px; height: 200px;" src="imgs/amigos-vetor">
+
+							</form> 
+			
+
+			
+			
+
+						<?php
+					}
+
+					if(isset($_POST['adicionar'])){
+
+						$text_add = $_POST['apelido'];
+
+						$apelido = $_POST['apelido'];
+
+						$sql_amigo = 'SELECT * FROM contas WHERE id = "'.$_SESSION['id'].'";';
+						$sql_exec = mysqli_query($conexao, $sql_amigo);
+						$sql_result = mysqli_fetch_array($sql_exec);
+
+
+						$apelido_conta = $sql_result['apelido'];
+
+						$sql_amigo = 'SELECT * FROM contas WHERE apelido LIKE "'.$apelido.'%";';
+						$sql_exec = mysqli_query($conexao, $sql_amigo);
+						$sql_result = mysqli_fetch_array($sql_exec);
+
+						$id_amigo = $sql_result['id'];
+						$apelido_amigo = $sql_result['apelido'];
+
+							// --------------------------------------
+
+						$sql_amigo = 'SELECT * FROM notificacao INNER JOIN contas ON notificacao.id_amigo = contas.id where id_amigo = "'.$id_amigo.'";';
+						$sql_exec = mysqli_query($conexao, $sql_amigo);
+						$sql_result = mysqli_fetch_array($sql_exec);
+
+						$linhas_soliacita = mysqli_num_rows($sql_exec);
+
+							//----------------------------------------------
+
+						$sql_amigo = 'SELECT * FROM amigos INNER JOIN contas ON amigos.id_amigo = contas.id where apelido_amigo = "'.$apelido_conta.'" and id_amigo = "'.$id_amigo.'";';
+						$sql_exec = mysqli_query($conexao, $sql_amigo);
+						$sql_result = mysqli_fetch_array($sql_exec);
+
+						$linhas_amigos = mysqli_num_rows($sql_exec);
+
+							//---------------------------------------------------
+
+						if(empty($text_add)){
+							echo ('<script>window.alert("Digite algum apelido!");window.location="index.php";</script>');
+						}else{
+
+							if($apelido == $apelido_conta){
+								echo ('<script>window.alert("Você não pode se adicionar!");window.location="index.php";</script>');
+							}else{
+
+								if($linhas_soliacita > 0){
+									echo ('<script>window.alert("Solicitação está pendente!");window.location="index.php";</script>');
+								}else{
+
+									if($linhas_amigos > 0){
+										echo ('<script>window.alert("Você e '.$apelido.' já são amigos!");window.location="index.php";</script>');
+									}else{
+
+										$sql_amigo = 'SELECT * FROM notificacao INNER JOIN contas ON notificacao.id_amigo = contas.id where id_conta = "'.$_SESSION['id'].'";';
+										$sql_exec = mysqli_query($conexao, $sql_amigo);
+										$sql_result = mysqli_fetch_array($sql_exec);
+
+										$id_solicitacao = $sql_result['id_amigo'];
+
+										if ($id_amigo == $_SESSION['id']) {
+											echo ('<script>window.alert("Você não pode se adicionar!");window.location="index.php";</script>');
+										}else{
+
+											if ($id_amigo == null) {
+												echo ('<script>window.alert("O usuário não existe na tabela");window.location="index.php";</script>');
+											}else{
+
+												if ($id_amigo == $id_solicitacao) {
+													echo ('<script>window.alert("Solicitação já está pendente!");window.location="index.php";</script>');
+												}else{
+
+													if(empty($apelido_tab)){
+
+														$sql_notifica = 'INSERT INTO notificacao (id_conta, id_amigo, apelido) VALUES ("'.$_SESSION['id'].'", "'.$id_amigo.'", "'.$apelido_conta.'");';
+														$sql_exec = mysqli_query($conexao, $sql_notifica);
+
+														header('location:index.php');
+													}
+
+												}
+											}
+										}
+										}
+									}
+								}
+							}
+						}
+					?>
+
 
 			<?php if(!isset($_SESSION['id'])){ ?>
 			<section class="container-main">
--- pages/chats.php
+++ pages/chats.php
@@ -71,72 +71,143 @@ include('../conexao.php');
 				</script>		
 			</header>
 
+<?php
+
+	include('../conexao.php');
+
+	$sql_foto = 'SELECT * FROM amigos INNER JOIN fotos ON fotos.id_fotos = amigos.id_conta;';
+	$sql_exec_fotos = mysqli_query($conexao, $sql_foto);
+	$sql_result = mysqli_fetch_array($sql_exec_fotos);
+
+	$dir = "../fotos/";
+
+	$sql_amigos = 'SELECT * FROM fotos;';
+	$sql_exec_amigos = mysqli_query($conexao, $sql_amigos);
+
+
+	$sql_ver = 'SELECT * FROM amigos INNER JOIN contas ON amigos.id_amigo = contas.id where id_conta = "'.$_SESSION['id'].'";';
+	$sql_exec = mysqli_query($conexao, $sql_ver);
+
+	$linhas = mysqli_num_rows($sql_exec);
+
+?>
+
 			<section class="container-main">
 
 				<section class="container-amigos">
 
-					<section class="primeiro-dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p> <br>
+					
+						<?php
+						
+
+						while ($con = mysqli_fetch_array($sql_exec)){	
+
+
+							echo ('
+								
+									<section class="primeiro-dentro">
+										<img src="'.$dir.$con['fk_nome_fotos'].'" class="foto" style="padding-top: 15px;">
+										<p style="margin-left: 70px; margin-top: -55px;">'.$con['nome'].'</p>
+										<img src="../imgs/deslogar.png" class="img-dentro">
+											
+										
+									</section>
+									
+
+							');
+						}?>
 					</section>
 
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-
-					<section class="dentro">
-						<img src="../imgs/icon" class="foto">
-						<p style="margin-left: 70px; margin-top: -55px;">nome do usuário</p>
-					</section>
-				</section>
 
 				<section class="container-mensagens">
-					<section class="container-enviar-mensagens">
-						<form>
-						<input type="text" name="enviar-mensagem" class="enviar-mensagem">
-					</form>
-						
-					</section>
+
 					
-				</section>
+					
+				
+
+			<?php
+				if(isset($_GET['id'])){
+				$id_amigo = 3;
+
+					$sqlacc = "SELECT * FROM contas INNER JOIN fotos ON contas.id = fotos.id_fotos Where id= ". $_SESSION['id'] .";";
+				    $sql_exec = mysqli_query($conexao, $sqlacc);
+				    $sql_result = mysqli_fetch_array($sql_exec);
+
+				    $dir = "../fotos/";
+				    $nome_fotos = $sql_result['nome_fotos'];
+					$id_conta = $sql_result['id'];
+
+					$_SESSION['email'] = $sql_result['email'];
+
+					echo ('<table><tr><td></td></tr>'); //correto acima
+
+		        	$sql_code = 'SELECT * FROM chats WHERE id_mensagem ORDER BY id_mensagem ASC;';
+		        	$sql_exec = mysqli_query($conexao, $sql_code);
+		        	$sql_result = mysqli_fetch_array($sql_exec);
+
+					while ($con = mysqli_fetch_array($sql_exec)){
+						$nova_foto = $con['fk_nome_fotos'];
+						
+					//a linha abaixo puxa a foto de quem mandou a mensagem abaixo e mansagem
+					echo('<tr><td rowspan="2">'.'<img src="'.$dir.$nova_foto.'">'.'</td><td class="chat-nome">'.$con['fk_apelido'].'</td></tr><tr><td class="chat-mensagem">'.$con['mensagem'].'</td></tr>'); 			
+					}
+
+					echo ('</table>');
+
+				if (isset($_POST['enviar-mensagem'])) {
+
+				   $sqlacc = "SELECT * FROM contas INNER JOIN fotos ON contas.id = fotos.id_fotos Where id= ". $_SESSION['id'] .";";
+				    $sql_exec = mysqli_query($conexao, $sqlacc);
+				    $sql_result = mysqli_fetch_array($sql_exec);
+
+				    $dir = "..\fotos/";
+				    $nome_fotos = $sql_result['nome_fotos'];
+				    $apelido = $sql_result['apelido'];
+					$id_conta = $sql_result['id'];
+
+					$_SESSION['email'] = $sql_result['email'];
+					
+		    		// Recupera os dados dos campos
+					$mensagens = $_POST['mensagem'];
+					$_SESSION['nome'] = $sql_result['nome'];
+					 
+
+					$sqlcad = 'insert into chats(id_conta, id_amigo, mensagem, fk_nome_fotos, fk_apelido) values ("'.$id_conta.'", "'.$id_amigo.'", "'.$mensagens.'", "'.$nome_fotos.'","'.$apelido.'");';
+		            $sql_exec = mysqli_query($conexao, $sqlcad);
+
+		        	/*Foto de perfil e nome de perfil */
+		        	echo ('<table><tr><td></td></tr>'); //correto acima
+		        	
+		        	$sql_code = 'SELECT * FROM chats WHERE id_mensagem = 1;';
+		        	$sql_exec = mysqli_query($conexao, $sql_code);
+		        	$sql_result = mysqli_fetch_array($sql_exec);
+		        	$linhas = mysqli_num_rows($sql_exec);
+		        	$mensagem = $sql_result['mensagem'];
+
+
+					$nova_foto = $sql_result['fk_nome_fotos'];
+					
+
+					
+					
+
+					echo('<section style="margin-top: 300px;"><tr style="margin-top: 500px;"><td rowspan="2">'.'<img src="'.$dir.$nova_foto.'">'.'</td><td class="chat-nome">'.$sql_result['fk_apelido'].'</td></tr><tr><td class="chat-mensagem">'.$mensagem.'</td></tr></section>'); 			
+					
+
+					echo ('</table>');
+					}
+}
+	?>
+
+
+
+	<section class="container-enviar-mensagens">
+						<form>
+							<input type="text" name="enviar-mensagem" class="enviar-mensagem">
+						</form>
+					</section>
+	</section>
 
 			</section>
-
 </body>
 </html>
